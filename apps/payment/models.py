a655b662de69c8f3bb7f97212a37316d896b4936
smk950226 <smk950226@naver.com>
2018-07-24 19:40:58 +0900

    sponsor model 생성

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from apps.accounts.models import Artist
+
+User = get_user_model()
+
+
+class Sponsor(models.Model):
+    user = models.ForeignKey(User, verbose_name = '후원자', on_delete = models.CASCADE)
+    artist = models.ForeignKey(Artist, verbose_name = '아티스트', on_delete = models.CASCADE)
+    amount = models.PositiveIntegerField('결제 금액')
+    regist_dt = models.DateTimeField(auto_now_add=True)
